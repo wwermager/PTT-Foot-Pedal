0939e8bb8e891c136cbc3106902bcb801ebea345
Will Wermager <wwermager09@winona.edu>
2018-08-27 01:50:17 -0500

    Quick press working - Semi-functional hold

--- PTT-Foot-Pedal.ino
+++ PTT-Foot-Pedal.ino
@@ -25,14 +25,67 @@ void setup() {
 
 // Loop checks if pttPin is pulled low, then holds the predefined key.
 void loop() {
-  if (digitalRead(pttPin) == LOW) {
-    Keyboard.press(pttKey);
+  int pressType = keyPress();
+  if (pressType == 1) quickPress();
+  if (pressType == 2) holdKeyEvent(); 
+//if (pressType == 3) doubleClick(); //TODO
+}
+void quickPress(){
+  // Event 1
+  // Could be bound to something else useful later
+  if (digitalRead(ledPin) == LOW){
     digitalWrite(ledPin, HIGH);
   }
-  
-// When pedal switch is released, pttPin is pulled high releasing the keypress.
-  else {
-    Keyboard.releaseAll();
+  else{
     digitalWrite(ledPin, LOW);
   }
 }
+void holdKeyEvent() {
+  // Event 2
+  Keyboard.press(pttKey);
+  digitalWrite(ledPin, HIGH);
+  while(digitalRead(pttPin) == LOW){
+    //DO NOTHING
+  }
+  Keyboard.releaseAll();
+  digitalWrite(ledPin, LOW);
+}
+
+void doubleClick(){
+  //TODO
+}
+
+// Button timing variables
+int debounce = 20;          // ms debounce period to prevent flickering when pressing or releasing the button
+int holdTime = 250;        // ms hold period: how long to wait for press+hold event
+// Button variables
+boolean buttonVal = HIGH;   // value read from button
+boolean buttonLast = HIGH;  // buffered value of the button's previous state
+long downTime = -1;         // time the button was pressed down
+long upTime = -1;           // time the button was released
+
+int keyPress(){
+  // Event = 1 quick press i.e. accidental - anything less than 250 ms
+  // Event = 2 Press and hold - 250ms to actuate
+  // Event = 3 Quick double-press - 2 clicks within 250 ms
+  int event = 0;
+   buttonVal = digitalRead(pttPin);
+   // Button pressed down
+
+   if (buttonVal == LOW && buttonLast == HIGH  && (millis() - upTime) > debounce){
+    downTime = millis();
+   }
+   // Button Released
+   else if (buttonVal == HIGH && buttonLast == LOW && (millis() - downTime) > debounce){
+    upTime = millis();
+    if (upTime - downTime < holdTime){
+      event = 1;
+    }
+   }
+   if (buttonVal == LOW && (millis() - downTime) >= holdTime){
+    event = 2;
+   }
+   buttonLast = buttonVal;
+   return event;
+}
+
