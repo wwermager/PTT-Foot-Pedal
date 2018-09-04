d0e44017ef521a882490f78dfcda2129d54582fd
Will Wermager <wwermager09@winona.edu>
2018-09-03 19:51:59 -0500

    Fixed flicker issue - added small delay to events

--- PTT-Foot-Pedal.ino
+++ PTT-Foot-Pedal.ino
@@ -1,13 +1,23 @@
-// Updated 27 August 2018 by Will Wermager - will@wermager.me
+// Updated 3 September 2018 by Will Wermager - will@wermager.me
 // Code is compatible only with ATmega32u4 based Arduinos such as the Leonardo or Micro.
-  
+
 #include <Keyboard.h>
-  int pttPin = 7;
-  int ledPin = 13;
+int pttPin = 7;
+int ledPin = 13;
 
 // Desired key to be pressed
 // Modifier keys can be found at: https://www.arduino.cc/reference/en/language/functions/usb/keyboard/keyboardmodifiers/
-  char pttKey = KEY_CAPS_LOCK;
+char pttKey = KEY_CAPS_LOCK;
+char quickKey = KEY_DOWN_ARROW; // bind for quick press
+
+// Button variables
+bool buttonVal = HIGH;  // value read from button
+bool buttonLast = HIGH; // buffered value of the button's previous state
+// Timing variables
+int debounce = 20;      // prevent flickering
+int holdTime = 500;     // ms hold period: how long to wait for press+hold event
+long downTime = -1;     // time the button was pressed down
+long upTime = -1;       // time the button was released
 
 void setup() {
   pinMode(pttPin, INPUT_PULLUP);
@@ -19,67 +29,63 @@ void setup() {
 void loop() {
   int pressType = keyPress();
   if (pressType == 1) quickPress();
-  if (pressType == 2) holdKeyEvent(); 
-//if (pressType == 3) doubleClick(); //TODO
+  if (pressType == 2) holdKeyEvent();
+  //if (pressType == 3) doubleClick(); //TODO
 }
-void quickPress(){
+
+void quickPress() {
   // Event 1
-  // Could be bound to something else useful later
-  if (digitalRead(ledPin) == LOW){
+  Keyboard.press(quickKey);
+  Keyboard.release(quickKey);
+  if (digitalRead(ledPin) == LOW) {
     digitalWrite(ledPin, HIGH);
   }
-  else{
+  else {
     digitalWrite(ledPin, LOW);
   }
+  delay(10); // Small delay to prevent event from being re-triggered immediately
 }
+bool pttPressed = false;
 void holdKeyEvent() {
   // Event 2
   Keyboard.press(pttKey);
   digitalWrite(ledPin, HIGH);
-  while(digitalRead(pttPin) == LOW){
+  while (digitalRead(pttPin) == LOW) {
     //DO NOTHING
   }
   Keyboard.releaseAll();
   digitalWrite(ledPin, LOW);
+  delay(10); // Small delay to prevent event from being re-triggered immediately
 }
 
-void doubleClick(){
-  //TODO
+void doubleClick() {
+  // TODO
 }
 
-// Button timing variables
-int debounce = 20;          // prevent flickering
-int holdTime = 250;        // ms hold period: how long to wait for press+hold event
-// Button variables
-boolean buttonVal = HIGH;   // value read from button
-boolean buttonLast = HIGH;  // buffered value of the button's previous state
-long downTime = -1;         // time the button was pressed down
-long upTime = -1;           // time the button was released
-
-int keyPress(){
+int keyPress() {
   // Event = 1 quick press i.e. accidental - anything less than 250 ms
   // Event = 2 Press and hold - 250ms to actuate
   // Event = 3 Quick double-press - 2 clicks within 250 ms
-  
-   int event = 0;
-   buttonVal = digitalRead(pttPin);
-   
-   // Button pressed down
-   if (buttonVal == LOW && buttonLast == HIGH  && (millis() - upTime) > debounce){
+
+  int event = 0;
+  buttonVal = digitalRead(pttPin);
+
+  // Button pressed down
+  if (buttonVal == LOW && buttonLast == HIGH  && (millis() - upTime) > debounce) {
     downTime = millis();
-   }
-   // Button Released
-   else if (buttonVal == HIGH && buttonLast == LOW && (millis() - downTime) > debounce){
+  }
+  // Button Released
+  else if (buttonVal == HIGH && buttonLast == LOW && (millis() - downTime) > debounce) {
     upTime = millis();
-    if (upTime - downTime < holdTime){
+    if (upTime - downTime < holdTime) {
       event = 1;
     }
-   }
-   // Check for press+hold
-   if (buttonVal == LOW && (millis() - downTime) >= holdTime){
+  }
+  // Check for press+hold
+  if (buttonVal == LOW && (millis() - downTime) >= holdTime) {
     event = 2;
-   }
-   buttonLast = buttonVal;
-   return event;
+  }
+  buttonLast = buttonVal;
+  return event;
 }
 
